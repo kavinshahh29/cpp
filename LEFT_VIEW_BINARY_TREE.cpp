ea05aa5f14bc8391c898231b510236dc3b2c284b
Kavin <kavinshah290304@gmail.com>
2023-10-21 03:01:30 +0530

    [LEFT VIEW OF BINARY TREE]

---
+++ LEFT_VIEW_BINARY_TREE.cpp
@@ -0,0 +1,31 @@
+vector<int> leftView(Node *node)
+{
+            vector<int>ans;
+          if(node==nullptr)
+          return ans;
+          vector<int>res;
+          queue<Node*>q;
+          q.push(node);
+          q.push(NULL);
+          while(!q.empty())
+          {
+              Node*temp=q.front();
+              q.pop();
+              if(temp==nullptr)
+              {
+                  res.push_back(ans[0]);
+                  ans.clear();
+                  if(!q.empty())
+                  q.push(NULL);
+              }
+              else
+              {
+                  if(temp->left!=nullptr)
+                  q.push(temp->left);
+                   if (temp->right)
+                  q.push(temp->right);
+                  ans.push_back(temp->data);
+              }
+          }
+          return res;
+}
