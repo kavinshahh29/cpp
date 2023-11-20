f742311ce462457fe89cbe1350f4a69eb9850dcd
Kavin <kavinshah290304@gmail.com>
2023-11-20 15:12:13 +0530

    [BFS APPROACH ADDED]

--- ALIEN_DICTATIONARY.cpp
+++ ALIEN_DICTATIONARY.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//    USING DFS
 
 string findOrder(string dict[], int N, int k) {
         vector<int>adj[k+1];
@@ -46,5 +46,77 @@ string findOrder(string dict[], int N, int k) {
         }
         char c=(char)(start+97);
         st.push(c);
+        
+    }
+
+
+
+
+
+    // USING BFS
+
+
+
+
+     string findOrder(string dict[], int N, int K) {
+        
+        vector<int>adj[K];
+        for(int i=0;i<N-1;i++)
+        {
+            for(int j=0;j<min(dict[i].length(),dict[i+1].length());j++)
+            {
+                if(dict[i][j]!=dict[i+1][j])
+                {
+                    adj[dict[i][j]-97].push_back({dict[i+1][j]-97});
+                    break;
+                }
+            }
+            
+        }
+        
+        vector<int>indegree(K,0);
+        for(int i=0;i<K;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<K;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
+            }
+        }
+        string ans="";
+        
+        while(!q.empty())
+        {
+            int start=q.front();
+            q.pop();
+            for(auto it:adj[start])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+           ans=ans+(char(97+start));
+        }
+        return ans;
+
+        
+        
+                
+        
+        
+        
+        
+        
+        
+        
         
     }
