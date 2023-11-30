727adb37467a32d99b59743edfb4380fbb8e5f17
Kavin <kavinshah290304@gmail.com>
2023-11-30 21:35:43 +0530

    [BFS APPROACH ADDED ]

--- PREREQUISITE_TASK_.cpp
+++ PREREQUISITE_TASK_.cpp
@@ -1,6 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+
+//  USING DFS
+
 bool isPossible(int N,int P, vector<pair<int, int> >& pre) {
 	    vector<int>adj[N+1];
 	    for(int i=0;i<pre.size();i++)
@@ -45,4 +49,56 @@ bool isPossible(int N,int P, vector<pair<int, int> >& pre) {
 	    }
 	    pathvisit[start]=false;
 	    return false;
+	}
+
+
+
+
+// USING BFS 
+
+
+
+bool isPossible(int n,int m, vector<pair<int, int> >& pre) {
+	    
+	     vector<int>adj[n];
+        for(int i=0;i<m;i++)
+        {
+            adj[pre[i].second].push_back(pre[i].first);
+            
+        }
+        vector<int>ans;
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(auto it:adj[i])
+            {
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push({i});
+            }
+        }
+        while(!q.empty())
+        {
+            int start=q.front();
+            q.pop();
+            for(auto it:adj[start])
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.push(it);
+                }
+            }
+            ans.push_back(start);
+        }
+        if(ans.size()!=n)
+        return false;
+        return true;
+        
 	}
