1e642b3075a7f6abd9902884ffeb9f91b964c334
Kavin <kavinshah290304@gmail.com>
2023-12-01 11:38:29 +0530

    [USING DISJOINT SET UNION APPROACH ADDED ]

--- NO_OF_PROVINCES_GRAPHS.cpp
+++ NO_OF_PROVINCES_GRAPHS.cpp
@@ -105,3 +105,87 @@ class Solution {
         
     }
 };
+
+
+
+// USING DISJOINT SET UNION 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int find(int node,vector<int>&parent)
+   {
+       if(node==parent[node])
+       return node;
+       
+       return parent[node]=find(parent[node],parent);
+   }
+   
+    int numProvinces(vector<vector<int>> adj, int V) {
+        vector<int>rank(V,0);
+        vector<int>parent(V);
+        for(int i=0;i<V;i++)
+        {
+            parent[i]=i;
+        }
+        vector<int>adja[V];
+        for(int i=0;i<adj.size();i++)
+        {
+            for(int j=0;j<adj[i].size();j++)
+            {
+                if(adj[i][j]==1 && i!=j)
+                {
+                    int ulp_i=find(i,parent);
+                    int ulp_j=find(j,parent);
+                    
+                    
+                    if(ulp_i!=ulp_j)
+                    {
+                        if(rank[ulp_i]>rank[ulp_j])
+                        {
+                            parent[ulp_j]=ulp_i;
+                            
+                        }
+                        else if(rank[ulp_j]>rank[ulp_i])
+                        {
+                            parent[ulp_i]=ulp_j;
+                        }
+                        else
+                        {
+                            parent[ulp_i]=ulp_j;
+                            rank[ulp_j]++;
+                        }
+                    }
+                   
+                }
+            }
+        }
+        // set<int>st;
+        // for(int i=0;i<V;i++)
+        // {
+        //     int p=find(i,parent);
+        //     st.insert(p);
+        // }
+        // return st.size();
+        int c=0;
+        for(int i=0;i<V;i++)
+        {
+            if(find(i,parent)==i)
+            c++;
+        }
+        return c;
+        
+    }
