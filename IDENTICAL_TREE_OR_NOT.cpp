be0fdd63294a032edf255023a3ef207a45a1c5ff
Kavin <kavinshah290304@gmail.com>
2024-05-13 16:07:21 +0530

    [Remove duplicate]

--- IDENTICAL_TREE_OR_NOT.cpp
+++
@@ -1,8 +0,0 @@
-bool isIdentical(Node *r1, Node *r2)
-    {
-        //Your Code here
-        if(r1==nullptr || r2==nullptr)
-        return r1==r2;
-        
-        return isIdentical(r1->left,r2->left) and isIdentical(r1->right,r2->right) and r1->data==r2->data;
-    }
